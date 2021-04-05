687cfe78c41dbf741e42c3d8646116ae596a02fe
David Castro <51448763+DavidCastroUFC@users.noreply.github.com>
2021-04-05 18:35:09 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import axios from 'axios';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Input,Space,Typography,Modal } from 'antd';
+const { Title } = Typography;
+const { Search } = Input;
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [data,setData] = useState();
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSearch = async (value)=>{
+   const res =  await axios.get(`https:\\viacep.com.br/ws/${value}/json/`); 
+   setData(res.data);
+   console.log(res.data)
+   showModal();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Space style={ window.innerWidth>450?{position:'absolute',left:'60%',top:'45%',display:'flex', justifyContent:'center',alignItems:'center'}:{position:'absolute',left:'5%',top:'26%',display:'flex', justifyContent:'center',alignItems:'center'}} direction="vertical">
+        <Title style={window.innerWidth>450?{fontSize:30,color:"#DB4644"}:{fontSize:26,color:"#F2DC16"}}>Consulte seu CEP aqui</Title>
+        <Search maxLength={9} style={{width:250}} placeholder="Digite o CEP" onSearch={onSearch} enterButton />
+      </Space>
+      {data!==undefined
+      ?
+      <Modal title="Endereço" visible={isModalVisible}  onCancel={handleCancel} footer={[]}>
+        <p>CEP: <strong>{data.cep}</strong></p>
+        <p>Logradouro: <strong>{data.logradouro}</strong></p>
+        <p>Complemento: <strong>{data.complemento}</strong></p>
+        <p>Bairro: <strong>{data.bairro}</strong></p>
+        <p>Localidade: <strong>{data.localidade}</strong></p>
+        <p>UF: <strong>{data.uf}</strong></p>
+      </Modal>
+      :
+      <></>
+    }
+    </div>  
   );
 }
 
